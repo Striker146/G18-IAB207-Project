d4295b426b88edeb1decb32108ccb698c1cd9ca9
Striker146 <gordenfreeman1@hotmail.com>
2023-09-26 16:59:18 +1000

    Created Booking.py and updated event.py

--- projectfile/website/booking.py
+++ projectfile/website/booking.py
@@ -1,21 +1,32 @@
+from user import User
+from event import Event
+
 class Booking:
     def __init__(self):
         self.id = None
         self.unique_identifier = None
         self.seats_booked = None
         self.purchase_date =  None
-        self.user = None
-        self.event = None
+        self.user:User = None
+        self.event:Event = None
         
-    def register(self,seats_booked,purchase_date,user,event):
+    def register(self,seats_booked,purchase_date,user:User, event : Event):
         self.seats_booked = seats_booked
         self.purchase_date = purchase_date
         self.user = user
         self.event = event
+        self.event.purchase_tickets(seats_booked)
         
     def __repr__(self):
-        str = f"id{self.id},unique_identifier:{self.unique_identifier},seats_booked:{self.seats_booked},purchased_date:{self.purchase_date},user:{self.user}, event:{self.event}"
+        str = f"id{self.id},unique_identifier:{self.unique_identifier},seats_booked:{self.seats_booked},purchased_date:{self.purchase_date},user:{self.user.id}, event:{self.event.id}"
         return str
     
     
-    
+if __name__ == "__main__":
+    test_booking = Booking()
+    test_user = User()
+    test_user.register("Hello", "blah blah blah","I am and a emailoo")
+    test_event = Event()
+    test_event.register("The scandaviation fligcer",test_user,"blah",40,"your house","Time of birth","date of birth","www.lmao.com.au",20,"enumer","DnD 5e",True,True,"low - mid")
+    test_booking.register(2,"now",test_user,test_event)
+    print(test_booking)
--- projectfile/website/event.py
+++ projectfile/website/event.py
@@ -1,8 +1,10 @@
+from user import User
+
 class Event:
     def __init__(self):
         self.id = None
         self.title = None
-        self.owner = None
+        self.owner:user.User = None
         self.description = None
         self.cost = None
         self.location = None
@@ -10,7 +12,8 @@ class Event:
         self.date = None
         self.image = None
         self.total_tickets = None
-        self.purchased_tickets = None
+        self.purchased_tickets = 0
+        self.remaining_tickets = None
         self.status = None
         self.age_range = None
         self.game_system = None
@@ -18,11 +21,11 @@ class Event:
         self.is_oneshot = None
         self.player_experience = None
         
-    def register(self, title,owner, description, cost, location, time,
-                 date, image, total_tickets, purchased_tickets, status,
-                 age_range, game_system, new_campaign, is_oneshot, player_experience):
+    def register(self, title,owner:User, description, cost, location, time,
+                 date, image, total_tickets,age_range, game_system,
+                 new_campaign, is_oneshot, player_experience):
         self.title = title
-        self.owner = owner
+        self.owner:User = owner
         self.description = description
         self.cost = cost
         self.location = location
@@ -30,8 +33,7 @@ class Event:
         self.date = date
         self.image = image
         self.total_tickets = total_tickets
-        self.purchased_tickets = purchased_tickets
-        self.status = status
+        self.remaining_tickets = total_tickets
         self.age_range = age_range
         self.game_system = game_system
         self.new_campaign = new_campaign
@@ -39,11 +41,17 @@ class Event:
         self.player_experience = player_experience
 
     def __repr__(self):
-        str = f"""title:{self.title},owner:{self.owner},description:{self.description},cost:{self.cost},location:{self.location},time:{self.time},date:{self.date},image:{self.image},total_tickets{self.total_tickets}
+        str = f"""title:{self.title},owner:{self.owner.id},description:{self.description},cost:{self.cost},location:{self.location},time:{self.time},date:{self.date},image:{self.image},total_tickets{self.total_tickets}
         purchased_tickets:{self.purchased_tickets},status:{self.status},age_range:{self.age_range},game_system:{self.game_system},new_campaign:{self.new_campaign},is_oneshot{self.is_oneshot},player_experience{self.player_experience}"""
         return str
     
+    def purchase_tickets(self,amount):
+        self.purchased_tickets = self.purchased_tickets + amount
+        self.remaining_tickets = self.total_tickets - self.purchased_tickets
+    
 if __name__ == "__main__":
     test_event = Event()
-    test_event.register("a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a")
+    test_user = User()
+    test_user.register("a","a","a")
+    test_event.register("a",test_user,"a","a","a","a","a","a","a","a","a","a","a","a")
     print(test_event)
