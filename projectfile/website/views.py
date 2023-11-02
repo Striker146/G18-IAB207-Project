7b0300f406a00b5dbdef0fca17dcf9aa53cc8c17
Striker146 <gordenfreeman1@hotmail.com>
2023-11-02 17:12:50 +1000

    Fixed search form not exisintg

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,render_template, request, redirect, url_for
 from . import db
 from .models import Event, GameSystem
+from .forms import SearchForm
 
 bp = Blueprint('main', __name__)
 
@@ -8,10 +9,11 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     Event.compare_dates()
+    search_form = SearchForm()
     # Fetch the 4 events
     events = db.session.scalars(db.select(Event)).fetchmany(4)
     # Render the 'index.html' template with the list of events
-    return render_template('index.html', events=events)
+    return render_template('index.html', events=events,search_form=search_form)
 
 #@bp.route('/event_creation')
 #def event_creation():
